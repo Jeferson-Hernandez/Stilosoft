7b81d2f1578fb4173306eb6803c8eca9c5ab71c0
Jeferson <jhernandez0642@misena.edu.co>
2021-06-15 13:43:26 -0500

    Agregar archivos de proyecto.

---
+++ Stilosoft/Models/Entities/Producto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stilosoft.Models.Entities
+{
+    public class Producto
+    {
+    }
+}
