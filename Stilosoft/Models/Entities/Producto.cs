be4670929bf322987120a9914e78d905cdb2515d
DanielP <danielpolo210@gmail.com>
2021-06-15 14:28:42 -0500

    Se creo la entidad producto y C,A,B

--- Stilosoft/Models/Entities/Producto.cs
+++ Stilosoft/Models/Entities/Producto.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,23 @@ namespace Stilosoft.Models.Entities
 {
     public class Producto
     {
+        [Key]
+        public int ProductoId { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [Column(TypeName = "nvarchar(50)")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "La cantidad es obligatoria")]
+        public int Cantidad { get; set; }
+
+        [Required(ErrorMessage = "La categoria es obligatoria")]
+        [Column(TypeName = "nvarchar(50)")]
+        public string Categoria { get; set; }
+
+        [Required(ErrorMessage = "El precio es obligatorio")]
+        public long Precio { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string RutaImagen { get; set; }
     }
 }
