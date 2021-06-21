2957518faa2663142d0a5dd319cdc633c0c5f40c
Jeferson <jhernandez0642@misena.edu.co>
2021-06-21 01:00:19 -0500

    CRUD Servicios faltan algunas validaciones

--- Stilosoft/Controllers/ServiciosController.cs
+++ Stilosoft/Controllers/ServiciosController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Stilosoft.Business.Abstract;
+using Stilosoft.Model.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,87 @@ namespace Stilosoft.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _servicioService.ObtenerListaServicios());
+        }
+
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear(Servicio servicio)
+        {
+            if (ModelState.IsValid)
+            {
+                servicio.Estado = true;
+
+                try
+                {
+                    await _servicioService.GuardarServicio(servicio);
+                    return RedirectToAction("index");
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Editar(int id)
+        {
+            Servicio servicio = await _servicioService.ObtenerServicioPorId(id);
+            return View(servicio);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Editar(Servicio servicio)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _servicioService.EditarServicio(servicio);
+                    return RedirectToAction("index");
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Eliminar(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _servicioService.EliminarServicio(id);
+                    return RedirectToAction("index");
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            else
+            {
+                return NotFound();
+            }
         }
     }
 }
