afd9701ba99f2d2e1034cc8ed0e43df9e9371fa9
Jeferson <jhernandez0642@misena.edu.co>
2021-06-26 13:56:10 -0500

    Crear usuarios por parte del administrador

--- Stilosoft/Controllers/UsuariosController.cs
+++ Stilosoft/Controllers/UsuariosController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Stilosoft.Business.Abstract;
 using Stilosoft.Model.Entities;
@@ -18,19 +19,21 @@ namespace Stilosoft.Controllers
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly RoleManager<IdentityRole> _roleManager;
         const string SesionNombre = "_Nombre";
 
-        public UsuariosController(IClienteService clienteService, UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, IHttpContextAccessor httpContextAccessor)
+        public UsuariosController(IClienteService clienteService, UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, IHttpContextAccessor httpContextAccessor, RoleManager<IdentityRole> roleManager)
         {
             _clienteService = clienteService;
             _userManager = userManager;
             _signInManager = signInManager;
             _httpContextAccessor = httpContextAccessor;
+            _roleManager = roleManager;
         }
         public async Task<IActionResult> Index()
         {
             var listaUsuariosClientes = await _clienteService.ObtenerListaClientes();
-            //var listaUsuarios = await _userManager.Users.Include(c=>c.Cliente).ToListAsync();
+            //var listaUsuarios = await _userManager.Users.Include(c=>c.Cliente).ToListAsync();            
             return View(listaUsuariosClientes);
         }
         [HttpGet]
@@ -56,7 +59,7 @@ namespace Stilosoft.Controllers
                     if (resultado.Succeeded)
                     {
                         var usuario = await _userManager.FindByEmailAsync(usuarioViewModel.Email);
-                        
+                        await _userManager.AddToRoleAsync(usuario, "cliente");
                         Cliente cliente = new()
                         {
                             ClienteId = usuario.Id,
@@ -67,14 +70,13 @@ namespace Stilosoft.Controllers
                             Estado = true                            
                         };
                         await _clienteService.GuardarCliente(cliente);
-                        return RedirectToAction("index", "Servicios");
+                        return RedirectToAction("login", "Usuarios");
                     }
                     else
                         return View(usuarioViewModel);
                 }
                 catch (Exception)
                 {
-
                     throw;
                 }
             }
@@ -105,6 +107,52 @@ namespace Stilosoft.Controllers
                 return View();
             }
             return View(loginViewModel);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CrearUsuario()
+        {
+            //var listaRoles = await _roleManager.Roles.ToListAsync();
+            var listaRoles = await _roleManager.Roles.Where(r => r.Name != "Administrador").ToListAsync();
+            ViewBag.Roles = new SelectList(listaRoles, "Name", "Name");
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CrearUsuario(CrearUsuarioViewModel crearUsuarioViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser identityUser = new()
+                {
+                    UserName = crearUsuarioViewModel.Email,
+                    Email = crearUsuarioViewModel.Email
+                };
+                try
+                {
+                    var resultado = await _userManager.CreateAsync(identityUser, crearUsuarioViewModel.Password);
+                    if (resultado.Succeeded)
+                    {
+                        var usuario = await _userManager.FindByEmailAsync(crearUsuarioViewModel.Email);
+                        await _userManager.AddToRoleAsync(usuario, crearUsuarioViewModel.Rol);
+
+                        return RedirectToAction("index");
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            return View(crearUsuarioViewModel);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CerrarSesion()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Login");
         }
     }
 }
---
+++ Stilosoft/ViewModels/Usuarios/CrearUsuarioViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stilosoft.ViewModels.Usuarios
+{
+    public class CrearUsuarioViewModel
+    {        
+        [Required(ErrorMessage = "El email es obligatorio")]
+        [EmailAddress(ErrorMessage = "Email invalido")]
+        public string Email { get; set; }
+        [DisplayName("Contraseña")]
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "El rol es obligatorio")]
+        public string Rol { get; set; }
+    }
+}
