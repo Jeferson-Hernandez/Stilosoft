983f5939693cd1715e9339f77958bb27e843650c
Daniel <daniel323agudelo@gmail.com>
2021-06-30 22:31:02 -0500

    Restaurar contraseña

---
+++ Stilosoft.Business/Dtos/Usuarios/OlvidePasswordDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stilosoft.Business.Dtos.Usuarios
+{
+    public class OlvidePasswordDto 
+    {
+        [Required(ErrorMessage = "El email es requerido")]
+        [EmailAddress(ErrorMessage = "Email invalido")]
+        public string Email { get; set; }
+    }
+}
+
