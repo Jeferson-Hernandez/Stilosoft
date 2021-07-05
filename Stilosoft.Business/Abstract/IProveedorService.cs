8061edd2cbb26c91ab7b3a1cbf5ca1f3e1ea3d2d
Guillermo <gr2@misena.edu.co>
2021-07-05 15:32:07 -0500

    Cambios proveedor

--- Stilosoft.Business/Abstract/IProveedorService.cs
+++ Stilosoft.Business/Abstract/IProveedorService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Stilosoft.Model.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,6 +7,12 @@ using System.Threading.Tasks;
 namespace Stilosoft.Business.Abstract
 {
     interface IProveedorService
-    {
+    {        
+        Task<IEnumerable<Proveedor>> ObtenerListaProveedor();
+        Task<Proveedor> ObtenerProveedorPorId(int id);
+        Task GuardarProveedor(Proveedor proveedor);
+        Task EditarProveedor(Proveedor proveedor);
+        Task EliminarProveedor(int id);
+        Task<Proveedor> NombreProveedorExiste(string nombre);
     }
 }
--- Stilosoft.Business/Business/ProveedorService.cs
+++ Stilosoft.Business/Business/ProveedorService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Stilosoft.Model.DAL;
+using Stilosoft.Model.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +10,47 @@ namespace Stilosoft.Business.Business
 {
     public class ProveedorService
     {
+        private readonly AppDbContext _context;
+
+        public ProveedorService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Proveedor>> ObtenerListaProveedor()
+        {
+            return await _context.Proveedor.ToListAsync();
+        }
+
+        public async Task<Proveedor> ObtenerProveedorPorId(int id)
+        {
+            return await _context.Proveedor.FirstOrDefaultAsync(s => s.ProveedorId == id);
+        }
+
+        public async Task GuardarProveedor(Proveedor proveedor)
+        {
+            _context.Add(proveedor);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task EditarProveedor(Proveedor proveedor)
+        {
+            _context.Update(proveedor);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task EliminarProveedor(int id)
+        {
+            var proveedor = await ObtenerProveedorPorId(id);
+            _context.Remove(proveedor);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Proveedor> NombreProveedorExiste(string nombre)
+        {
+            return await _context.Proveedor.FirstOrDefaultAsync(n => n.Nombre == nombre);
+        }
     }
 }
+    
+
