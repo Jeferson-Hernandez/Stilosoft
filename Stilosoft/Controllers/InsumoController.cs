c677bf28e0eb8fa0abbcc54ee5ed61092466d8a9
Daniel <daniel323agudelo@gmail.com>
2021-07-11 23:20:49 -0500

    Cambios controlador insumos

--- Stilosoft/Controllers/InsumoController.cs
+++ Stilosoft/Controllers/InsumoController.cs
@@ -59,6 +59,16 @@ namespace Stilosoft.Controllers
 
                     if (insumoExiste != null)
                     {
+                        
+                            TempData["Accion"] = "Error";
+                            TempData["Mensaje"] = "El insumo ya existe";
+                        return RedirectToAction("Index");
+                    }
+
+                    if (insumoViewModel.Cantidad <= 0)
+                    {
+                        TempData["Accion"] = "Error";
+                        TempData["Mensaje"] = "La cantidad no puede ser menor o igual a 0";
                         return RedirectToAction("Index");
                     }
                     await _insumoService.RegistrarInsumo(insumo);
@@ -115,6 +125,12 @@ namespace Stilosoft.Controllers
 
                 try
                 {
+                    if (insumoViewModel.Cantidad <= 0)
+                    {
+                        TempData["Accion"] = "Error";
+                        TempData["Mensaje"] = "La cantidad no puede ser menor o igual a 0";
+                        return RedirectToAction("Index");
+                    }
                     /*var insumoExiste = await _insumoService.NombreInsumoExiste(insumo.Nombre);
 
                     if (insumoExiste != null)
