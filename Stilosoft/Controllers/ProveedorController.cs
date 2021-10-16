c24dd0ed89d624d3f2db9b7dc1ce80f12a86606a
Jeferson <jhernandez0642@misena.edu.co>
2021-10-16 11:07:42 -0500

    Agr vistas sin funcionalidad

--- Stilosoft/Controllers/ProveedorController.cs
+++ Stilosoft/Controllers/ProveedorController.cs
@@ -55,7 +55,7 @@ namespace Stilosoft.Controllers
                     }                    
                     await _proveedorService.GuardarProveedor(proveedor);
                     TempData["Accion"] = "Crear";
-                    TempData["Mensaje"] = "El NIT se ha registrado";
+                    TempData["Mensaje"] = "Proveedor registrado";
                     return RedirectToAction("index");
                 }
                 catch (Exception)
@@ -98,6 +98,7 @@ namespace Stilosoft.Controllers
             {
                 Proveedor proveedor = new()
                 {
+                    ProveedorId = proveedorViewModels.ProveedorId,
                     Nit = proveedorViewModels.Nit,
                     Nombre = proveedorViewModels.Nombre,
                     Direccion = proveedorViewModels.Direccion,
