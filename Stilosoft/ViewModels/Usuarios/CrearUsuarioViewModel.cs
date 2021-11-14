2bd4ae437d3b07f95c492915e7078e503a13e3dc
DanielP <danielpolo210@gmail.com>
2021-11-14 12:11:13 -0500

    AbonoCompra-AlgunosDetalles

--- Stilosoft/ViewModels/Usuarios/CrearUsuarioViewModel.cs
+++ Stilosoft/ViewModels/Usuarios/CrearUsuarioViewModel.cs
@@ -10,17 +10,21 @@ namespace Stilosoft.ViewModels.Usuarios
 {
     public class CrearUsuarioViewModel
     {
+        [StringLength(50, ErrorMessage = "Máximo 50 caracteres")]
         public string Nombre { get; set; }
         [Required(ErrorMessage = "El apellido es obligatorio")]
         [Column(TypeName = "nvarchar(50)")]
+        [StringLength(50, ErrorMessage = "Máximo 50 caracteres")]
         public string Apellido { get; set; }
         [DisplayName("Número")]
         [Required(ErrorMessage = "El número es obligatorio")]
         [Column(TypeName = "nvarchar(10)")]
+        [StringLength(10, ErrorMessage = "Máximo 10 caracteres")]
         public string Numero { get; set; }
         [DisplayName("Documento")]
         [Required(ErrorMessage = "El documento es obligatorio")]
         [Column(TypeName = "nvarchar(15)")]
+        [StringLength(15, ErrorMessage = "Máximo 15 caracteres")]
         public string Documento { get; set; }
         [Required(ErrorMessage = "El email es obligatorio")]
         [EmailAddress(ErrorMessage = "Email invalido")]
