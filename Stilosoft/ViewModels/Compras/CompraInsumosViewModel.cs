46b92f5417903a50754d5a98303cce8e94a49368
Jeferson <jhernandez0642@misena.edu.co>
2021-11-15 22:01:14 -0500

    Cambios al agregar productos

---
+++ Stilosoft/ViewModels/Compras/CompraInsumosViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Stilosoft.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stilosoft.ViewModels.Compras
+{
+    public class CompraInsumosViewModel
+    {
+        public int compraId { get; set; }
+        public List<ListaInsumos> CompraInsumos { get; set; }
+    }
+
+    public class ListaInsumos
+    {
+        public int ProductoId { get; set; }
+        public int Cantidad { get; set; }
+        public long Costo { get; set; }
+        public long SubTotal { get; set; }
+        public int Iva { get; set; }
+        public long Total { get; set; }
+    }
+}
