4761d46f07879d72f2f601b2f087eae1cc74d6ca
DanielP <danielpolo210@gmail.com>
2021-11-16 11:09:58 -0500

    AlgunosDetallesAbonoCompra

--- Stilosoft.Model/Entities/AbonoCompra.cs
+++ Stilosoft.Model/Entities/AbonoCompra.cs
@@ -15,14 +15,19 @@ namespace Stilosoft.Model.Entities
         public int AbonoCompraId { get; set; }
         [ForeignKey("Compra")]
         public int CompraId { get; set; }
+        [DisplayName("Cantidad abonada")]
         public int CantAbono { get; set; }
         [Column(TypeName = "Date")]
+        [DisplayName("Fecha de pago")]
         public DateTime FechaPago { get; set; }
         
         [Required(ErrorMessage = "Las cuotas son obligatorias")]
         public int Cuotas { get; set; }
+        [DisplayName("Valor inicial")]
         public long ValorInicial { get; set; }
+        [DisplayName("Valor final")]
         public long ValorFinal { get; set; }
+        
         public virtual Compra Compra { get; set; }
         public virtual DetalleCompra DetalleCompra { get; set; }
 
---
+++ Stilosoft.Model/Migrations/20211114192139_Abono.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Stilosoft.Model.Migrations
+{
+    public partial class Abono : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "MontoAbonandoTotal",
+                table: "AbonoCompra",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MontoAbonandoTotal",
+                table: "AbonoCompra");
+        }
+    }
+}
